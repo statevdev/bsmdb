c713c617a5dae86b16c4836686e676e09d3f78f7
edgerunner <rtate14@proton.me>
2024-07-19 04:56:42 +0600

    added manual startup pageupd.py and fixed a bug in the def push_to_github

--- pageupd.py
+++ pageupd.py
@@ -1,6 +1,7 @@
 import git
 from apscheduler.schedulers.background import BackgroundScheduler
 from expdata import ExportData
+from bot_settings import pageupdconfig
 
 
 class GithubPageUpdater:
@@ -14,9 +15,16 @@ class GithubPageUpdater:
         repo = git.Repo(self.local_repo)
         repo.git.add(list(self.html_files.values()))
         repo.index.commit(self.commit_message)
+
+        current_branch = repo.active_branch
+        tracking_branch = current_branch.tracking_branch()
+
+        if tracking_branch is None:
+            repo.git.branch('--set-upstream-to=origin/main', current_branch.name)
+
         repo.remotes.origin.push()
 
-    def add_job(self, at_hour, at_minutes):
+    def _add_job(self, at_hour, at_minutes):
         scheduler = BackgroundScheduler()
         scheduler.add_job(
             self.push_to_github, 'cron',
@@ -26,8 +34,16 @@ class GithubPageUpdater:
         return scheduler
 
     def run(self, hour, minutes):
-        for table_name, output_file in self.html_files.items():
-            ExportData.export_to_html(self.database_path, table_name, output_file)
-        scheduler = self.add_job(at_hour=hour, at_minutes=minutes)
+        self.htmls_creator()
+        scheduler = self._add_job(at_hour=hour, at_minutes=minutes)
         return scheduler
 
+    def htmls_creator(self):
+        for table_name, output_file in self.html_files.items():
+            ExportData.export_to_html(self.database_path, table_name, output_file)
+
+
+if __name__ == '__main__':
+    updater = GithubPageUpdater(**pageupdconfig)
+    updater.htmls_creator()
+    updater.push_to_github()
