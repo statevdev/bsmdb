9603ad92ecbc6dbc7c846bd930362bf51c8d191f
a-s-t-a-t-i-u-m <vasetanec@gmail.com>
2024-09-09 22:20:44 +0600

    docs for crypt_data.py

--- crypt_data.py
+++ crypt_data.py
@@ -7,12 +7,39 @@ except ImportError:
 
 
 class Crypt:
+    """
+    Класс для шифрования и дешифрования данных с использованием Fernet.
+    """
     @staticmethod
-    def decrypt_data(data):
+    def decrypt_data(data: str) -> str:
+        """
+        Дешифрует зашифрованные данные.
+
+        Этот метод принимает зашифрованные данные в виде строки и возвращает
+        их в расшифрованном виде.
+
+        Параметры:
+        data (str): Зашифрованные данные в виде строки.
+
+        Возвращает:
+        str: Расшифрованные данные в виде строки.
+        """
         fernet = Fernet(config['db']['key'])
         return fernet.decrypt(data.encode()).decode()
 
     @staticmethod
-    async def encrypt_data(*args):
+    async def encrypt_data(*args: str) -> tuple:
+        """
+        Шифрует данные.
+
+        Этот метод принимает произвольное количество аргументов и возвращает
+        их в зашифрованном виде в виде кортежа.
+
+        Параметры:
+        *args (str): Данные для шифрования.
+
+        Возвращает:
+        tuple: Кортеж зашифрованных данных.
+        """
         fernet = Fernet(config['db']['key'])
         return tuple(fernet.encrypt(str(arg).encode()).decode() for arg in args)
