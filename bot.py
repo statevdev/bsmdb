3f8ff4e9072e94d3a2c6146ad84317156fd7dc8d
a-s-t-a-t-i-u-m <vasetanec@gmail.com>
2024-09-09 21:57:03 +0600

    docs for bot.py

--- bot.py
+++ bot.py
@@ -16,15 +16,44 @@ logging.basicConfig(
 
 
 class Bot:
-    def __init__(self, token):
+    def __init__(self, token: str) -> None:
+        """
+        Инициализация бота.
+
+        Этот метод создает экземпляр бота и привязывает команды к приложению.
+
+        Параметры:
+        token (str): Токен для аутентификации бота в API.
+
+        Возвращает:
+        None: Этот метод ничего не возвращает.
+        """
         self.application = ApplicationBuilder().token(token).build()
         self.commands = CommandsFactory.create_commands(self.application)
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Запускает бота и начинает слушать сообщения от пользователей.
+
+        Этот метод инициирует процесс опроса, который позволяет боту получать и обрабатывать входящие сообщения.
+
+        Возвращает:
+        None: Этот метод ничего не возвращает.
+        """
         return self.application.run_polling()
 
 
-def main():
+def main() -> None:
+    """
+    Главная функция приложения.
+
+    Эта функция инициализирует базу данных, настраивает и запускает обновление страницы на GitHub,
+    а также инициализирует и запускает бота Telegram. Она удерживает главный поток живым,
+    чтобы позволить планировщику работать, пока бот активен.
+
+    Возвращает:
+    None: Эта функция ничего не возвращает.
+    """
     # Инициализация базы данных
     database = BotDatabase(config['db']['database_path'])
     database.create_tables()
